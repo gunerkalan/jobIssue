95ab4d4fc28f2b3f74f717ea361c445968f75a14
Güner KALAN <gunerkalan@gmail.com>
2023-05-10 22:37:24 +0300

    add integration tests project

---
+++ account/src/test/java/com/guner/account/IntegrationTestSupport.java
@@ -0,0 +1,34 @@
+package com.guner.account;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.guner.account.dto.converter.CustomerDtoConverter;
+import com.guner.account.repository.CustomerRepository;
+import com.guner.account.service.AccountService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+public abstract class IntegrationTestSupport extends TestSupport {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Autowired
+    public CustomerRepository customerRepository;
+
+    @Autowired
+    public AccountService accountService;
+
+    @Autowired
+    public CustomerDtoConverter customerDtoConverter;
+
+    public final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    public void setup(){
+        mapper.registerModule(new JavaTimeModule());
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    }
+}
